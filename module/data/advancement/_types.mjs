b760acdac44b2568aefc063881c58ad093503827
Jeff Hitchcock <mail@command-center.org>
2023-02-12 13:59:58 -0800

    [#2187] Add usage scale value type
    Adds a usage scale value type that tracks number of uses and the
    recovery period. This can be used to set the uses of an item and
    will display better in the class journal page.
    
    Additional work needs to be done to allow for changing the recvoery
    period based on the scale value.

--- module/data/advancement/_types.mjs
+++ module/data/advancement/_types.mjs
@@ -109,6 +109,12 @@
  */
 
 /**
+ * @typedef ScaleValueUsesTypeData
+ * @property {number} value           Number of uses.
+ * @property {string} per             Usage refresh period.
+ */
+
+/**
  * Information on how a scale value of this type is configured.
  *
  * @typedef ScaleValueTypeMetadata
