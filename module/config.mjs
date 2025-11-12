b531c608bb57bd5102188f51e5dc26628935f4d2
Jeff Hitchcock <mail@command-center.org>
2025-11-12 10:39:00 -0800

    [#6335] Add `ModifyItem` advancement for enchanting existing items
    Adds a new advancement type that allows for applying an enchantment
    to an existing item on an actor's sheet. This advancement contains
    a list of enchantments to apply, and a list of identifiers that are
    used to find the items to apply those enchantments to.
    
    During the advancement process, each enchantment will be displayed
    with a list of matching items below it.
    
     ### Future Improvement
    - Allow user control over what items are actually enchanted
    - Support rider activities & effects once #6357 is resolved
    
    Closes #6335

--- module/config.mjs
+++ module/config.mjs
@@ -4424,6 +4424,10 @@ DND5E.advancementTypes = {
     documentClass: advancement.ItemGrantAdvancement,
     validItemTypes: new Set(_ALL_ITEM_TYPES)
   },
+  ModifyItem: {
+    documentClass: advancement.ModifyItemAdvancement,
+    validItemTypes: new Set(_ALL_ITEM_TYPES)
+  },
   ScaleValue: {
     documentClass: advancement.ScaleValueAdvancement,
     validItemTypes: new Set(_ALL_ITEM_TYPES)
